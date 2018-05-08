59d19ed9ba964fe4329c81ee77ff81c32553b8bc
zhangshuying <zhangshuying@zhangshyingsmbp.domain>
2018-05-07 23:28:53 -0700

    project3 initial commit

---
+++ src/main/java/datastructures/concrete/ArrayHeap.java
@@ -0,0 +1,145 @@
+package datastructures.concrete;
+
+import datastructures.interfaces.IPriorityQueue;
+import misc.exceptions.EmptyContainerException;
+
+/**
+ * See IPriorityQueue for details on what each method must do.
+ */
+public class ArrayHeap<T extends Comparable<T>> implements IPriorityQueue<T> {
+    // See spec: you must implement a implement a 4-heap.
+    private static final int NUM_CHILDREN = 4;
+
+    // You MUST use this field to store the contents of your heap.
+    // You may NOT rename this field: we will be inspecting it within
+    // our private tests.
+    private T[] heap;
+
+    // Feel free to add more fields and constants.
+    private int cap = 100;
+    private int currentSize;
+
+    public ArrayHeap() {
+        this.heap = makeArrayOfT(cap);
+        this.currentSize = 0;
+    }
+
+    /**
+     * This method will return a new, empty array of the given size
+     * that can contain elements of type T.
+     *
+     * Note that each element in the array will initially be null.
+     */
+    @SuppressWarnings("unchecked")
+    private T[] makeArrayOfT(int size) {
+        // This helper method is basically the same one we gave you
+        // in ArrayDictionary and ChainedHashDictionary.
+        //
+        // As before, you do not need to understand how this method
+        // works, and should not modify it in any way.
+        return (T[]) (new Comparable[size]);
+    }
+
+    @Override
+    public T removeMin() {
+        T item = null;
+        if (this.currentSize == 0) {
+            throw new EmptyContainerException();
+        } else if (this.currentSize == 1) {
+            item = this.heap[0];
+            this.heap = null;
+            this.currentSize--;
+        } else {
+            item = this.heap[0];
+            this.heap[0] = this.heap[currentSize - 1];
+            this.currentSize--;
+            //find its four children, compare and swap (recursion)
+            //Base case: smaller than all children
+            // 4i + n, n-th child
+            this.percolateDown(0);
+        }
+        return item;
+    }
+
+    @Override
+    public T peekMin() {
+        if (this.currentSize == 0) {
+            throw new EmptyContainerException();
+        } else {
+            return this.heap[0];
+        }
+    }
+
+    @Override
+    public void insert(T item) {
+        if (item == null) {
+            throw new IllegalArgumentException();
+        } else {
+            //put at the end
+            this.heap[currentSize] = item;
+            this.currentSize++;
+            //find its parent compare and swap (recursion)
+            //Base Case: greater than its parents
+            //(fourth children: i/4 - 1; i/4 otherwise)
+            this.percolateUp(currentSize - 1);
+        }
+    }
+
+    @Override
+    public int size() {
+        return this.currentSize;
+    }
+    
+    private void percolateUp(int i) {
+        //find its parent
+        int parent;
+        if (i % 4 == 0) {
+            parent = i/4 - 1;
+        } else {
+            parent = i/4;
+        }
+        if (parent < 0) {
+            return;
+        } else if (this.heap[parent].compareTo(this.heap[i]) <= 0) {
+            return;
+        } else { 
+            T temp = this.heap[i];
+            this.heap[i] = this.heap[parent];
+            this.heap[parent] = temp;
+            this.percolateUp(parent);
+        }
+    }
+    
+    private void percolateDown(int i) {
+        int child;
+        // k - 1 : number of children
+        child = findMinChild(i);
+        if (child == i) {
+            return;
+        } else {
+            T temp = this.heap[i];
+            this.heap[i] = this.heap[child];
+            this.heap[child] = temp;
+            this.percolateDown(child);
+        }
+    }
+    
+    private int findMinChild(int i) {
+        int child;
+        T min = this.heap[i];
+        int minIndex = i;
+        for (int k = 1; k <= 4; k++) {
+            child = 4 * i + k; 
+            if (this.heap[child] != null) {
+                if (min.compareTo(this.heap[child]) > 1) {
+                    min = this.heap[child];
+                    minIndex = child;
+                }
+            } else {
+                break;
+            }
+        }
+        return minIndex;
+    }
+    
+}
---
+++ src/main/java/datastructures/concrete/dictionaries/ChainedHashDictionary.java
@@ -0,0 +1,217 @@
+package datastructures.concrete.dictionaries;
+
+import datastructures.concrete.KVPair;
+import datastructures.interfaces.IDictionary;
+import misc.exceptions.NoSuchKeyException;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+
+/**
+ * See the spec and IDictionary for more details on what each method should do
+ */
+public class ChainedHashDictionary<K, V> implements IDictionary<K, V> {
+    // You may not change or rename this field: we will be inspecting
+    // it using our private tests.
+    private IDictionary<K, V>[] chains;
+    private int cap;
+    private int num;
+    
+    // You're encouraged to add extra fields (and helper methods) though!
+
+    public ChainedHashDictionary() {
+        num = 0; // initial size
+        cap = 9973; // pick a prime for capacity of initial array 
+        chains = makeArrayOfChains(cap);
+    }
+
+    /**
+     * This method will return a new, empty array of the given size
+     * that can contain IDictionary<K, V> objects.
+     *
+     * Note that each element in the array will initially be null.
+     */
+    @SuppressWarnings("unchecked")
+    private IDictionary<K, V>[] makeArrayOfChains(int size) {
+        // Note: You do not need to modify this method.
+        // See ArrayDictionary's makeArrayOfPairs(...) method for
+        // more background on why we need this method.
+        return (IDictionary<K, V>[]) new IDictionary[size];
+    }
+    
+    public int getHash(K key) {
+        if (key == null) {
+            return 0;
+        } else {
+            return Math.abs(key.hashCode() % this.cap);
+        }
+    }
+
+    @Override
+    public V get(K key) {
+        int hash = this.getHash(key);
+        if (chains[hash] == null) {
+            throw new NoSuchKeyException();
+        }
+        return chains[hash].get(key);
+        
+    }
+
+    @Override
+    public void put(K key, V value) {
+        int hash = this.getHash(key);
+        if (chains[hash] == null) {
+            chains[hash] = new ArrayDictionary<K, V>();
+        }        
+        if (!chains[hash].containsKey(key)) {
+            this.num++;
+        }
+        chains[hash].put(key, value);
+        // resize();
+    }
+    
+    // resize the array only if lambda > 1
+    public void resize() {
+        if (this.num > this.cap) {       
+            this.cap = this.cap * 2;
+            IDictionary<K, V>[] newChains = makeArrayOfChains(cap);
+            for (KVPair<K, V> pair : this) {
+                K key = pair.getKey();
+                V value = pair.getValue();
+                int newindex = this.getHash(key);
+                if (newChains[newindex] == null) {
+                    newChains[newindex] = new ArrayDictionary<K, V>();
+                }
+                newChains[newindex].put(key, value);
+            }
+            chains = newChains;          
+        }
+    }
+
+    @Override
+    public V remove(K key) {
+        int hash = this.getHash(key);
+        if (chains[hash] == null) {
+            throw new NoSuchKeyException();
+        } else {
+            if (chains[hash].containsKey(key)) {
+                this.num--;
+            }
+            return chains[hash].remove(key);
+        }
+    }
+
+    @Override
+    public boolean containsKey(K key) {
+        int hash = this.getHash(key);
+        if (chains[hash] == null) {
+            return false;
+        } else {
+            return chains[hash].containsKey(key);
+        }
+    }
+
+    @Override
+    public int size() {
+        return this.num;
+    }
+
+    @Override
+    public Iterator<KVPair<K, V>> iterator() {
+        // Note: you do not need to change this method
+        return new ChainedIterator<>(this.chains);
+    }
+
+    /**
+     * Hints:
+     *
+     * 1. You should add extra fields to keep track of your iteration
+     *    state. You can add as many fields as you want. If it helps,
+     *    our reference implementation uses three (including the one we
+     *    gave you).
+     *
+     * 2. Before you try and write code, try designing an algorithm
+     *    using pencil and paper and run through a few examples by hand.
+     *
+     *    We STRONGLY recommend you spend some time doing this before
+     *    coding. Getting the invariants correct can be tricky, and
+     *    running through your proposed algorithm using pencil and
+     *    paper is a good way of helping you iron them out.
+     *
+     * 3. Think about what exactly your *invariants* are. As a
+     *    reminder, an *invariant* is something that must *always* be 
+     *    true once the constructor is done setting up the class AND 
+     *    must *always* be true both before and after you call any 
+     *    method in your class.
+     *
+     *    Once you've decided, write them down in a comment somewhere to
+     *    help you remember.
+     *
+     *    You may also find it useful to write a helper method that checks
+     *    your invariants and throws an exception if they're violated.
+     *    You can then call this helper method at the start and end of each
+     *    method if you're running into issues while debugging.
+     *
+     *    (Be sure to delete this method once your iterator is fully working.)
+     *
+     * Implementation restrictions:
+     *
+     * 1. You **MAY NOT** create any new data structures. Iterators
+     *    are meant to be lightweight and so should not be copying
+     *    the data contained in your dictionary to some other data
+     *    structure.
+     *
+     * 2. You **MAY** call the `.iterator()` method on each IDictionary
+     *    instance inside your 'chains' array, however.
+     */
+    private static class ChainedIterator<K, V> implements Iterator<KVPair<K, V>> {
+        private IDictionary<K, V>[] chains;
+        private int index;
+        private Iterator<KVPair<K, V>> currIter;
+
+        public ChainedIterator(IDictionary<K, V>[] chains) {
+            this.chains = chains;
+            this.index = 0;
+            this.currIter = this.moveIter();
+        }
+        
+        private Iterator<KVPair<K, V>> moveIter() {
+                while (index < chains.length && chains[index] == null) {
+                    this.index++;
+                }
+                if (index == chains.length) {
+                    return null;
+                } else {
+                    return chains[index].iterator();
+                }
+        } 
+        
+        @Override
+        public boolean hasNext() {
+            if (this.currIter == null) {
+                return false;
+            } else if (this.index == chains.length - 1) {
+                return currIter.hasNext();
+            } else {
+                if (!currIter.hasNext()) {
+                    this.index++;
+                    this.currIter = this.moveIter();
+                    if (this.currIter == null) {
+                        return false;
+                    }  
+                }
+                return true;
+            }
+        }
+
+        @Override
+        public KVPair<K, V> next() {
+            if (hasNext()) {
+                return currIter.next();
+            } else {
+                throw new NoSuchElementException();
+            } 
+        }           
+    }
+}
